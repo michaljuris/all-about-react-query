10d9e7701abf0785648990d21da142bd950b73fb
Michal Juris <michaljuris@gmail.com>
2021-03-17 22:06:27 +0100

    Add post initial data from posts cache

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
-import { useQuery } from 'react-query'
+import { useQuery, queryCache } from 'react-query'
 
 export const fetchPost = (postId) =>
   axios.get(`/api/posts/${postId}`).then((res) => res.data)
 
 export default function usePost(postId) {
-  return useQuery(['posts', postId], () => fetchPost(postId))
+  return useQuery(['posts', postId], () => fetchPost(postId), {
+    initialData: () => {
+      return queryCache.getQueryData('posts')?.find((d) => d.id == postId)
+    },
+    initialStale: true,
+  })
 }
