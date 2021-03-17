a006911637d79e5d227444bfe4876f1c1c143178
Michal Juris <michaljuris@gmail.com>
2021-03-17 19:15:39 +0100

    migrate usePost and usePosts to react-query

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,27 +1,11 @@
 import React from 'react'
 import axios from 'axios'
+import { useQuery } from 'react-query'
 
 export default function usePosts() {
-  const [state, setState] = React.useReducer((_, action) => action, {
-    isLoading: true,
-  })
+  const query = useQuery('posts', () =>
+    axios.get('/api/posts').then((res) => res.data)
+  )
 
-  const fetch = async () => {
-    setState({ isLoading: true })
-    try {
-      const data = await axios.get('/api/posts').then((res) => res.data)
-      setState({ isSuccess: true, data })
-    } catch (error) {
-      setState({ isError: true, error })
-    }
-  }
-
-  React.useEffect(() => {
-    fetch()
-  }, [])
-
-  return {
-    ...state,
-    fetch,
-  }
+  return query
 }
