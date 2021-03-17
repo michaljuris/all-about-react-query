ab483e37f7b0a249c0d75d27fa01085637393a86
Michal Juris <michaljuris@gmail.com>
2021-03-17 23:02:50 +0100

    migrate useCreatePost and useSavePost to react-query useMigration

--- src/hooks/useSavePost.js
+++ src/hooks/useSavePost.js
@@ -1,22 +1,14 @@
-import React from 'react'
 import axios from 'axios'
+import { queryCache, useMutation } from 'react-query'
 
 export default function useSavePost() {
-  const [state, setState] = React.useReducer((_, action) => action, {
-    isIdle: true,
-  })
-
-  const mutate = React.useCallback(async (values) => {
-    setState({ isLoading: true })
-    try {
-      const data = await axios
-        .patch(`/api/posts/${values.id}`, values)
-        .then((res) => res.data)
-      setState({ isSuccess: true, data })
-    } catch (error) {
-      setState({ isError: true, error })
+  return useMutation(
+    (values) =>
+      axios.patch(`/api/posts/${values.id}`, values).then((res) => res.data),
+    {
+      onSuccess: (data, values) => {
+        queryCache.invalidateQueries(['posts', data.id])
+      },
     }
-  }, [])
-
-  return [mutate, state]
+  )
 }
